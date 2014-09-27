ab8de0e4f8098e2303d2c7d1ee95a0897c9f4e2d
Isis Anchalee <isiswenger@gmail.com>
2014-09-26 17:00:42 -0700

    meow

---
+++ README.md
@@ -0,0 +1,3 @@
+#One Month Rails
+
+meow meow meow
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kittenterestings::Application.config.secret_key_base = '3255ae0182cbf63186361db01cbc9af411eeaf1532b0d7401e678d017600225bb23e82396e3c57868ab717bdb62f84cfda1ec5e4672fe1df6d514efe98538736'
